d887b86f8f9b126f7a5ddc62801feb6ca5d98346
AndreiMercieca <andrei23mercieca@gmail.com>
2022-05-23 17:39:00 +0200

    Added projects and new java script file

---
+++ GRD51/Example/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+  
+    var $menu = $('#menu'),
+      $menulink = $('.menu-link');
+  
+    $menulink.click(function() {
+      $menulink.toggleClass('active');
+      $menu.toggleClass('active');
+      toggleButtonText($(this));
+      return false;
+    });
+  
+    function toggleButtonText(element) {
+      if (element.html() == 'CLOSE') {
+        element.html('MENU');
+      } else {
+        element.html('CLOSE');
+      }
+    }
+
+  
+    $('.item').mouseenter(function() {
+      $(this).find('.item-info').fadeIn(400);
+      $(this).css("cursor", "pointer");
+    });
+  
+    $('.item').mouseleave(function() {
+      $(this).find('.item-info').fadeOut(400)
+    });
+  
+  });
+
+ 
