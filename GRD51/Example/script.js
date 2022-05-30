bdf9402da7b85cec868cadca3cf9691368c3997b
AndreiMercieca <andrei23mercieca@gmail.com>
2022-05-30 14:34:05 +0200

    Validated all the pages of html and css

--- GRD51/Example/script.js
+++ GRD51/Example/script.js
@@ -1,37 +1,36 @@
 $(document).ready(function() {
-
   
-    var $menu = $('#menu'),
-      $menulink = $('.menu-link');
   
-    $menulink.click(function() {
-      $menulink.toggleClass('active');
-      $menu.toggleClass('active');
-      toggleButtonText($(this));
-      return false;
-    });
+  var $menu = $('#menu'),
+  $menulink = $('.menu-link');
   
-    function toggleButtonText(element) {
-      if (element.html() == 'CLOSE') {
-        element.html('MENU');
-      } else {
-        element.html('CLOSE');
-      }
-    }
-
-  
-    $('.item').mouseenter(function() {
-      $(this).find('.item-info').fadeIn(300);
-      $(this).css("cursor", "pointer");
-    });
-  
-    $('.item').mouseleave(function() {
-      $(this).find('.item-info').fadeOut(500)
-    });
-  
+  $menulink.click(function() {
+    $menulink.toggleClass('active');
+    $menu.toggleClass('active');
+    toggleButtonText($(this));
+    return false;
   });
-
   
+  function toggleButtonText(element) {
+    if (element.html() == 'CLOSE') {
+      element.html('MENU');
+    } else {
+      element.html('CLOSE');
+    }
+  }
+  
+  
+  $('.item').mouseenter(function() {
+    $(this).find('.item-info').fadeIn(300);
+    $(this).css("cursor", "pointer");
+  });
+  
+  $('.item').mouseleave(function() {
+    $(this).find('.item-info').fadeOut(500)
+  });
+  
+});
 
 
- 
+
+
